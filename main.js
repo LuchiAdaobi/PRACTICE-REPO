e1701387553c5e17efb80037375b82b7a59bc246
LuchiAdaobi <Luchichronicles@gmail.com>
2022-03-21 23:45:17 +0100

    rewrote javascript for password gen

--- main.js
+++ main.js
@@ -1,73 +1,91 @@
-// Dom select the form id
-// create the fetch function 
+// Select DOM elementsconst 
+const resultEl = document.querySelector('#result');
+const lengthEl = document.querySelector('#length');
+const lowercaseEl = document.querySelector('#lowercase');
+const uppercaseEl = document.querySelector('#uppercase');
+const numbersEl = document.querySelector('#numbers');
+const symbolsEl = document.querySelector('#symbols');
+const generateEl =  document.querySelector('#generate');
+const clipboardEl = document.querySelector('#clipboard');
 
-function getLocationInfo(e){
-    e.preventDefault()
-    const zip = document.querySelector('.zip').value
+// Create get random password function
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+function getRandomNumber(){
+    return Math.floor(Math.random() * 10)
+}
+function getRandomSymbol(){
+    const symbols = `!@#$%^&*(){}[]=<>/,.`
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
 
-    fetch(`http://api.zippopotam.us/us/${zip}`).then(
-        (response) => {
-        if(response.status !== 200){
-            showIcon('remove')
-            document.querySelector('#output').innerHTML = 
-            ` <article class="message is-danger">
-            <div class = "message-body">Invalid Zipcode, please try again</div></article>`
-            
-            throw error(response.statusText)
-        } else {
-            showIcon('check')
-            return response.json()
-        }
-        } 
-    ).then((data) => {
-        let output = '';
-        data.places.forEach((place) => {
-            output += `
-            <article class="message is-primary">
-            <div class="message-header">
-            <p> Location Info </p>
-            <button class = "delete"></button>
-            </div>
-            <div class="message-body">
-            <ul>
-            <li><strong> City: </strong> ${place['place name']}</li>
-            <li><strong> State: </strong> ${place.state}</li>
-            <li><strong> Longitude: </strong> ${place.longitude}</li>
-            <li><strong> Latitude: </strong> ${place.latitude}</li>
-            </div>
-            </article>`
-     
+const randFunc = {
+    upper: getRandomUpper,
+    lower: getRandomLower,
+    number: getRandomNumber,
+    symbol: getRandomSymbol, 
+}
+// add click event to generate button
+generateEl.addEventListener('click', () =>{
+    length = +lengthEl.value;
+
+    hasUpper = uppercaseEl.checked
+    hasLower = lowercaseEl.checked
+    hasNumber = numbersEl.checked
+    hasSymbol = symbolsEl.checked
+
+    resultEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length)
+})
+// create the generate password function
+
+function generatePassword(lower, upper, number, symbol, length){
+    // initialise the generated password
+    const generatedPassword = ''
+
+    // get the count
+    const typeCount = upper + lower + number + symbol
+    // filter the checked
+    const typeArr = [{upper}, {lower}, {number}, {symbol}].filter((item) => 
+         Object.values(item)[0]
+    )
+
+    // check if any of the boxes are checked
+    if(typeCount === 0){
+        return ''
+    }
+    // loop through each to return random function
+
+    for(i = 0; i < length; i+=typeCount){
+        typeArr.forEach((type) =>{
+            const funcName = Object.keys(type)[0]
+            generatedPassword+= randFunc[funcName]()
         })
-        
-        document.querySelector('#output').innerHTML = output
-    }).catch((err) => console.log(err));
-
 
     }
+    const finalPassword = generatedPassword.slice(0, length).split('').sort(() => Math.random() - 0.5).join('')
 
+    return finalPassword
 
-
-// icon functions
-function showIcon(icon){
-
-    // clear
-    document.querySelector('.icon-remove').style.display = 'none'
-    document.querySelector('.icon-check').style.display = 'none'
-
-    document.querySelector(`.icon-${icon}`).style.display = 'inline-flex'
-}
-
-// delete function
-function deleteLocation(e){
-    if(e.target.className === 'delete'){
-        document.querySelector('.message').remove()
-        document.querySelector('.zip').value = ''
-        document.querySelector('.icon-check').style.display = 'none'
-
+    for(let i = 0; i < 
     }
 }
-document.querySelector('body').addEventListener('click', deleteLocation)
+// create the clipboard function
+clipboardEl.addEventListener('click', () =>{
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innertext
 
-// call it on submit
-document.querySelector('#zipForm').addEventListener('submit', getLocationInfo)
+    if(!password){
+        return ''
+    }
 
+    textarea = password.value
+    document.body.appendChild = textarea
+    textarea.select()
+    document.execCommand('copy')
+    document.alert('copied to clipboard')
+    textarea.remove()
+})
